f9613346f891c9a412c1f46c52f1091adb388c0f
Ivann <youenn.tillard@outlook.com>
2019-10-30 12:45:50 +0100

    correction on atoi sign

--- the_library/libft/ft_atoi.c
+++ the_library/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: yotillar <yotillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 03:19:04 by yotillar          #+#    #+#             */
-/*   Updated: 2019/10/07 03:20:23 by yotillar         ###   ########.fr       */
+/*   Updated: 2019/10/30 12:43:19 by yotillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,16 @@ int	ft_atoi(char *str)
 {
 	int i;
 	int res;
-	int sign;
 
-	sign = 0;
 	i = 0;
 	res = 0;
 	while ((*str > 8 && *str < 14) || *str == ' ')
 		str++;
-	while (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign++;
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-	}
 	while (str[i] >= '0' && str[i] <= '9')
 		res = res * 10 + (str[i++] - '0');
-	if (sign % 2 != 0)
+	if (*str == '-')
 		return (-res);
 	return (res);
 }
